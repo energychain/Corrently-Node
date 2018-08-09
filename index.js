480dbb5090a5bbee75d1846545ba339f20a0c1e9
Thorsten Zoerner <me@thorsten-zoerner.com>
2018-08-09 23:47:19 +0200

    Added PubSub

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+Error.stackTraceLimit = Infinity;
+require('dotenv').config();
 const IPFS = require('ipfs')
 const OrbitDB = require('orbit-db')
 
@@ -10,31 +12,60 @@ const ipfsOptions = {
 const ipfs = new IPFS(ipfsOptions)
 const ANNOUNCEMENT_CHANNEL="/orbitdb/QmTuydQHGjzgmcrupdG1yXjAxk372b3GT445iBfSR3jLC5/annoucement";
 
+var publish=async function(kv) {
+  var value=new Date();
+  kv.set("Performance",{updated:value});
+}
+
+const subscribePeer=async function(peer) {
+  const kv = await orbitdb.keyvalue('correnlty-performance');
+  await kv.load();
+  kv.events.on('replicated', (address) => {
+      console.log("Peer Event",peer,address);
+  });
+}
+
+const subscribeAnnouncements=async function(kv) {
+      const orbitdb = new OrbitDB(ipfs);
+      const announcement = await orbitdb.log(ANNOUNCEMENT_CHANNEL);
+      var subscribtions={};
+
+      var announceThis=function() {
+        announcement.add({peer:kv.address.toString(),signature:"signed"});
+      };
+
+      setInterval(announceThis,60000);
+      announceThis();
+
+      console.log("Announcement Channel",announcement.address.toString());
+
+      announcement.events.on('replicated', (address) => {
+        const items = announcement.iterator({ limit: 100 })
+          .collect()
+          .map((e) => e.payload.value);
+          for(var i=0;i<items.length;i++) {
+            if(typeof subscribtions[items[i].peer] == "undefined") {
+              subscribtions[items[i].peer]=subscribePeer(items[i].peer);
+            }
+          }
+      })
+}
+
 ipfs.on('error', (e) => console.error(e))
 ipfs.on('ready', async () => {
-  ipfs.swarm.connect("/ip4/52.59.191.11/tcp/4002/ipfs/QmcDy1vs1U39AG6Ls5XqTqwamdsyWkrTcgVYzJtAyou78j");
+  ipfs.swarm.connect("/ip4/52.59.191.11/tcp/4002/ipfs/QmcDy1vs1U39AG6Ls5XqTqwamdsyWkrTcgVYzJtAyou78j").catch(function() {})
   const orbitdb = new OrbitDB(ipfs)
-  const kv = await orbitdb.keyvalue('correnlty-performance');
+  const kv = await orbitdb.keyvalue('correnlty');
   await kv.load();
   // Listen for updates from peers
   kv.events.on('replicated', (address) => {
 
-  })
-  const access = {
-      // Give write access to everyone
-      write: ['*'],
-  }
-
-  const announcement = await orbitdb.log(ANNOUNCEMENT_CHANNEL);
-  announcement.add({peer:kv.address.toString(),signature:"signed"});
-  console.log("Announcement Channel",announcement.address.toString());
-  console.log("Protected KV",kv.address.toString());
-  announcement.events.on('replicated', (address) => {
-    const all = announcement.iterator({ limit: 1 })
-      .collect()
-      .map((e) => e.payload.value);
-    console.log(all);
-      console.log("New Replicate",address);
-  })
+  });
+  console.log("Peer KV",kv.address.toString());
+  subscribeAnnouncements(kv);
+  publish(kv);
+  setInterval(function() {
+      publish(kv);
+  },2000);
 
 })
