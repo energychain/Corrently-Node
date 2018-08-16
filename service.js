3ca5c96e57ea63c1145552a0dd9913131aaa15ce
Thorsten Zoerner <me@thorsten-zoerner.com>
2018-08-16 02:15:53 +0200

    Complete Refactoring of service class

--- service.js
+++ service.js
@@ -24,245 +24,150 @@ module.exports = async function(cbmain) {
 
   var init = require("./init.js");
   init(async function(cb) {
+      Error.stackTraceLimit = Infinity;
+      require('dotenv').config();
 
-        Error.stackTraceLimit = Infinity;
-        require('dotenv').config();
-        const IPFS = require('ipfs')
-        const OrbitDB = require('orbit-db')
-        const fs = require("fs");
-        const ethers = require("ethers");
-        const PouchDB = require('pouchdb');
-        const localPouch = PouchDB.defaults({prefix: process.env.DATADIR});
-        const express = require('express');
-        const wallet = new ethers.Wallet(process.env.PRIVATEKEY);
+      const IPFS = require('ipfs')
+      const OrbitDB = require('orbit-db')
+      const fs = require("fs");
+      const ethers = require("ethers");
+      const PouchDB = require('pouchdb');
+      PouchDB.plugin(require('pouchdb-upsert'));
+      const localPouch = PouchDB.defaults({prefix: process.env.DATADIR});
+      const express = require('express');
+      const wallet = new ethers.Wallet(process.env.PRIVATEKEY);
+      var ipfs_peers = [];
+      var ipfs_peers_transient=[];
 
-        const ipfsOptions = {
-          EXPERIMENTAL: {
-            pubsub: true
-          }
-        }
-        logger.info("Authority Node Address "+process.env.ACCOUNT);
+      const ipfsOptions = {
+         EXPERIMENTAL: {
+           pubsub: true
+         }
+      }
+      logger.info("Authority of Node "+process.env.ACCOUNT);
 
-        const ipfs = new IPFS(ipfsOptions)
-        const ANNOUNCEMENT_CHANNEL=process.env.ANNOUNCEMENT_CHANNEL;
-        var subscribtions={};
-
-        const publish=async function(kv,change) {
-          var nodedb = new localPouch(process.env.ACCOUNT);
-          if(typeof change=="undefined"){
-              nodedb.get(process.env.NODECLASS).then(async function(obj) {
-                // Add Swarm Info to main DATA Doc
-                obj.swarm={};
-                var peers=process.env.SWARM.split(",");
-                obj.swarm.PEERS=peers;
-                obj.swarm.IDLE_REPUBLISH=process.env.IDLE_REPUBLISH;
-                obj.swarm.IDLE_ANNOUNCEMENT=process.env.IDLE_ANNOUNCEMENT;
-                obj.swarm.E20CONTRACT=process.env.E20CONTRACT;
-                obj.swarm.SWARM_RECONNECT=process.env.SWARM_RECONNECT;
-                obj.swarm.SWARM_ID=process.env.IPFS_ID;
-                obj._publishTimeStamp=new Date();
-                await kv.set(obj._id,obj);
-            }).catch(async function(err) {
-              logger.info("Try publish without document? - we create an epmty document" + err);
-              var obj={};
-              obj.swarm={};
-              var peers=process.env.SWARM.split(",");
-              obj.swarm.PEERS=peers;
-              obj.swarm.IDLE_REPUBLISH=process.env.IDLE_REPUBLISH;
-              obj.swarm.IDLE_ANNOUNCEMENT=process.env.IDLE_ANNOUNCEMENT;
-              obj.swarm.E20CONTRACT=process.env.E20CONTRACT;
-              obj.swarm.SWARM_RECONNECT=process.env.SWARM_RECONNECT;
-              obj.swarm.SWARM_ID=process.env.IPFS_ID;
-              obj._id=process.env.NODECLASS;
-              await nodedb.put(obj);
-            });
-          } else {
-            const orbitdb = new OrbitDB(ipfs);
-            const announcement = await orbitdb.log(ANNOUNCEMENT_CHANNEL);
-           	const kv2 = await orbitdb.keyvalue(change._id);
-           	await kv2.load();
-            var signature=wallet.signMessage(kv2.address.toString());
-            announcement.add({peer:kv2.address.toString(),signature:signature,account:wallet.address,doc:change._id,swarm:process.env.IPFS_ID,test:"ZOERNER"});
-            logger.info("Pubslished " +change._id);
-            change._publishTimeStamp=new Date();
-            await kv.set(change._id,change);
-            setTimeout(function() {
-              publish(kv,change)
-            },process.env.IDLE_REPUBLISH);
-          }
-        }
-
-        const subscribePeer=async function(item) {
-          var peer = item.peer;
-          var e20abi=[  {"constant": true,"inputs": [{"name": "_owner","type": "address"}],"name": "balanceOf","outputs": [{"name": "balance","type": "uint256"}],"payable": false,"type": "function"}];
-          var contract = new ethers.Contract(process.env.E20CONTRACT, e20abi,ethers.providers.getDefaultProvider("homestead"));
-          if(typeof item.swarm != "undefined") {
-                ipfs.swarm.connect(item.swarm).then(function() { logger.info("Connected Swarm Peer "+item.swarm); connectPeer();}).catch(function() { logger.info("Failed Swarm Peer "+item.swarm);});
-          }
-          contract.balanceOf(item.account).then(async function(balance) {
-            var sign_address = ethers.Wallet.verifyMessage(item.peer, item.signature);
-              if((balance>0)&&(sign_address==item.account)&&(sign_address!=wallet.address)) {
-                if(typeof item.doc == "undefined") return;
-
-                logger.info("Added Peer " + item.account+ " "+item.doc);
-                const orbitdb = new OrbitDB(ipfs);
-                const kv = await orbitdb.keyvalue(peer);
-                await kv.load();
-                kv.events.on('replicated', (address) => {
-
-                    const v = kv.get(item.doc);
-
-                    if(typeof v == "undefined") {
-                      console.log("******* HARD Missing ",item.account,item.doc,peer);
-                      return;
-                    }
-                    console.log("*********",address,v._publishTimeStamp);
-                    item.doc=v.doc;
-
-                    if(typeof v._publishTimeStamp!="undefined") {
-                        v.publishTimeStamp=v._publishTimeStamp;
-                        delete v._publishTimeStamp;
-                    }
-                    logger.info("Updated "+peer +" "+item.account,item.doc);
-
-                    var doc= process.env.NODECLASS;
-                    if(typeof item.doc != "undefined") doc = item.doc;
-                    if(typeof v._id != "undefined") doc=v._id;
-
-                    var db = new localPouch(item.account);
-                    db.get(doc).then(function(dbdoc) {
-                      v._rev=dbdoc._rev;
-                      v._id=item.doc;
-                      return db.put(v).catch(function(e) {
-
-                      });
-                    }).then(function(response) {
-                      db.compact().then(function (result) {
-                      }).catch(function (err) {
-                      });
-                    }).catch(function (err) {
-                      if(typeof v._rev != "undefined") delete v._rev;
-                      return db.put(v).catch(function(e) {console.log("Insert",err,e,item.account,item.doc)});
-                    });
+      const ipfs = new IPFS(ipfsOptions)
+      const ANNOUNCEMENT_CHANNEL=process.env.ANNOUNCEMENT_CHANNEL;
+      ipfs.on('error', (e) => logger.error("IPFS"+e))
+      ipfs.on('ready', async () => {
+          const connectPeers = function() {
+            if(ipfs_peers_transient.length>0) {
+                var peer= ipfs_peers_transient.pop();
+                ipfs.swarm.connect(peer).then(function() {
+                  logger.info("Connected "+peer); connectPeers();
+                }).catch(function() {
+                  logger.info("Failed "+peer); connectPeers();
                 });
-              } else {
-                logger.info("Ignored peer ",item.account,item.peer);
-              }
-          });
-        }
-
-        const subscribeAnnouncements=async function(kv) {
-              const orbitdb = new OrbitDB(ipfs);
-              const announcement = await orbitdb.log(ANNOUNCEMENT_CHANNEL);
-
-              const refreshItems = function() {
-                const dbitems = announcement.iterator({ limit: -1,reverse:false,gt:"QmXnQpwKBXzBZZuVeFQ8kAGTAZ5g6kdG5QaxZrZe6DjcY9" })
-                  .collect();
-                  //console.log(dbitems);
-                  const items=dbitems.map((e) => e.payload.value);
-                  logger.info("Refreshing "+items.length+" Announcements");
-                  var swarmPeers=[];                  
-                  for(var i=0;i<items.length;i++) {
-                    if(typeof subscribtions[items[i].peer+"/"+items[i].doc] == "undefined") {
-                      logger.info("Monitor Peer "+items[i].peer+" doc "+items[i].doc);
-                      subscribtions[items[i].peer+"/"+items[i].doc]=items[i].account;
-                      subscribePeer(items[i]);
-
-                      if((typeof items[i] != "undefined")&&(typeof items[i].swarm != "undefined")) {
-                        swarmPeers.push(items[i].swarm)
-                      }
-                    }
-                  }
-                    var connectSwarms=function() {
-                          if(swarmPeers.length>0) {
-                              var peer = swarmPeers.pop();
-                              ipfs.swarm.connect(peer).then(function() {
-                                logger.info("Connected Swarm Peer "+peer); connectPeer();
-                                connectSwarms();
-                              }).catch(function() {
-                                logger.info("Failed Swarm Peer "+peer);
-                                connectSwarms();
-                              });
-                          }
-                      }
-                      connectSwarms();
-
-              }
-
-              var announceThis=function() {
-                var signature=wallet.signMessage(kv.address.toString());
-                announcement.add({peer:kv.address.toString(),signature:signature,account:wallet.address,doc:process.env.NODECLASS,swarm:process.env.IPFS_ID});
-              };
-
-              setInterval(announceThis,process.env.IDLE_ANNOUNCEMENT);
-              announceThis();
-
-              logger.info("Announcement Channel "+announcement.address.toString());
-
-              announcement.events.on('replicated', (address) => {
-                refreshItems();
-              })
-              refreshItems();
-        }
-
-        // Main Process starts here...
-
-        ipfs.on('error', (e) => console.error(e))
-        ipfs.on('ready', async () => {
-          logger.info("IPFS Daemon ready");
-          ipfs.id(function(err,id) {
-              process.env.IPFS_ID="/ip4/"+process.env.EXTERNAL_IP+"/tcp/4002/ipfs/"+id.id;
-              logger.info("IPFS ID "+process.env.IPFS_ID);
-          });
-
-          const app = express();
-          PouchDB.defaults({prefix: ''});
-          app.use('/', require('express-pouchdb')(localPouch,{inMemoryConfig:true}));
-          app.listen(process.env.POUCHDB_FAUXTON_PORT);
-
-          const connectPeers=async function() {
-
-            const peers=process.env.SWARM.split(",");
-
-            var connectPeer = function() {
-              if(peers.length>0) {
-                  var peer=peers.pop();
-                  ipfs.swarm.connect(peer).then(function() { logger.info("Connected "+peer); connectPeer();}).catch(function() { logger.info("Failed "+peer); connectPeer();});
-              }
             }
-            connectPeer();
           }
 
+         ipfs.id(function(err,id) {
+           process.env.IPFS_ID="/ip4/"+process.env.EXTERNAL_IP+"/tcp/4002/ipfs/"+id.id;
+           logger.info("IPFS ID of Node "+process.env.IPFS_ID);
 
-          setInterval(connectPeers,process.env.SWARM_RECONNECT);
-          connectPeers();
+         });
 
-          const orbitdb = new OrbitDB(ipfs)
-          const kv = await orbitdb.keyvalue(process.env.NODECLASS);
-          logger.info("Initialize local KV");
-          kv.load();
-          kv.events.on('replicated', (address) => {
-              logger.debug(address);
-          });
-          logger.info("Peer KV "+kv.address.toString());
-          subscribtions[kv.address.toString()]=function(){};
-          subscribeAnnouncements(kv);
-          publish(kv);
-          setInterval(function() {
-              publish(kv);
-          },process.env.IDLE_REPUBLISH);
-          var nodedb = new localPouch(wallet.address);
+         const orbitdb = new OrbitDB(ipfs)
+         const localdb = await orbitdb.docs("local");
+         const app = express();
+         PouchDB.defaults({prefix: ''});
+         app.use('/', require('express-pouchdb')(localPouch,{inMemoryConfig:true}));
+         app.listen(process.env.POUCHDB_FAUXTON_PORT);
+
+         // Ensure we have IPFS Peers we could talk too
+         ipfs_peers=process.env.SWARM.split(",");
+         ipfs_peers_transient=ipfs_peers;
+         connectPeers();
+         setInterval(function() {
+           ipfs_peers_transient=ipfs_peers;
+           connectPeers();
+         },process.env.SWARM_RECONNECT);
+
+         // Ensure we listen to annouuncements and are able to publish to
+          const announcement = await orbitdb.log(ANNOUNCEMENT_CHANNEL);
+          var orbitpeers=[];
+          announcement.events.on('replicated', () => {
+
+
+            var preProcessed = announcement.iterator({ limit: -1 }).collect().map(e => e.payload.value)
+
+            const processResults=function() {
+                if(result.length>0) {
+                    var item=result.pop();
+                    var e20abi=[  {"constant": true,"inputs": [{"name": "_owner","type": "address"}],"name": "balanceOf","outputs": [{"name": "balance","type": "uint256"}],"payable": false,"type": "function"}];
+                    var contract = new ethers.Contract(process.env.E20CONTRACT, e20abi,ethers.providers.getDefaultProvider("homestead"));
+                    contract.balanceOf(item.account).then(async function(balance) {
+                          var sign_address = ethers.Wallet.verifyMessage(item.peer, item.signature);
+                          if((balance>0)&&(sign_address==item.account)&&(sign_address!=wallet.address)) {
+                                ipfs.swarm.connect(item.swarm).then(function() {
+                                  logger.info("Announcement Connected "+item.swarm);
+                                }).catch(function() {
+                                  logger.info("Announcement Failed "+item.swarm);
+                                });
+                                ipfs_peers.push(item.swarm);
+
+                                 const remotedb = new localPouch(item.account);
+                                 const remoteorbit = await orbitdb.docs(item.peer).catch(function() {logger.info("Ignore legacy");});
+                                 if(typeof remoteorbit!="undefined") {
+                                   logger.info("Monitoring " + item.account+ " "+item.doc);
+                                   remoteorbit.events.on('replicated', () => {
+                                      const result = remoteorbit.iterator({ limit: -1 }).collect().map(e => e.payload.value)
+                                      logger.info("Remote Items "+item.account+" "+result.length);
+
+                                      function upsert() {
+                                        if(result.length>0) {
+                                          var doc=result.pop();
+                                          console.log("UPDATE",doc);
+                                          remotedb.upsert(doc._id,function(orgdoc) {
+                                              return doc;
+                                          }).then(function() {
+                                            upsert();
+                                          }).catch(function(e) {
+                                            logger.info("Upsert issue:"+e);
+                                            upsert();
+                                          });
+
+                                        }
+                                      }
+                                      if(result.length>0) upsert();
+
+                                    })
+                                }
+
+
+                          }
+                          processResults();
+                    });
+                }
+            }
+            var result=[];
+
+            for(var i=0;i<preProcessed.length;i++) {
+                  if(typeof orbitpeers[preProcessed[i].peer]=="undefined") {
+                    orbitpeers[preProcessed[i].peer]=preProcessed[i];
+                    result.push(preProcessed[i]);
+                  }
+            }
+
+            if(result.length>0) {
+              processResults();
+            }
+
+          })
+
+
+          // Ensure we have a local bound database
+          var nodedb = new localPouch("local");
           var nodechanges = nodedb.changes({
             since: 'now',
             live: true,
             include_docs: true
           }).on('change', function(change) {
-            publish(kv,change.doc);
+            logger.info("Local Document change: "+change.doc._id);
+            localdb.put(change.doc);
+            var signature=wallet.signMessage(localdb.address.toString());
+            announcement.add({peer:localdb.address.toString(),signature:signature,account:wallet.address,doc:change.doc._id,swarm:process.env.IPFS_ID});
           })
-          if(typeof cbmain =="function") {
-            cbmain(nodedb,kv);
-          }
-        })
-
+      });
   });
 }
