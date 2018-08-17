b4ddcf1c6c4b80ccb7120afa6fc5921faa7049cf
Thorsten Zoerner <me@thorsten-zoerner.com>
2018-08-17 02:24:29 +0200

    Added Command Line options to allow auto publish after file changes to distributed consens.

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Node for Corrently Assets
 
 Mainly this is a Proof of Stake implementation for (JSON) document sharing via IPFS.
 
-Goal of this module is to decentralize technical due diligence for Corrently business [https://corrently.com/] a micro invest case for renewable energy generation based on blockchain technology.
+Goal of this module is to decentralize technical due diligence for Corrently business https://corrently.com/ a micro invest case for renewable energy generation based on blockchain technology.
 
 ## Documentation
 Please refer to the Medium article: [https://medium.com/@zoernert/decentralized-due-diligence-serverless-dapp-5d042198960c]
@@ -15,3 +15,17 @@ npm install -g corrently-node
 
 $ corrently-node
 ```
+
+### Run as Daemon
+```
+corrently-node start
+corrently-node stop
+corrently-node restart
+```
+
+### Publish a file and sign it
+```
+corrently-node -j FILENAME.json -t DOCNAME
+```
+
+You could add `-m` to monitor for changes of file. File/Document will be automtically republished on change.
--- index.js
+++ index.js
@@ -2,11 +2,58 @@
 
 var startStopDaemon = require('start-stop-daemon');
 
+
 startStopDaemon({}, function() {
 
+  // Start service if not already alive
   var service=require("./service.js");
-  service(function() {
-    console.log("INIT Completed");
-  });
-  
+  const fs = require("fs");
+
+  require('dotenv').config();
+
+  const PouchDB = require('pouchdb');
+  const localPouch = PouchDB.defaults();
+  localPouch.plugin(require('pouchdb-upsert'));
+  var nodedb = new localPouch("http://localhost:"+process.env.POUCHDB_FAUXTON_PORT+"/local");
+
+  const bootstrap=function() {
+          nodedb.info().then(function (result) {
+            const optionDefinitions = [
+              { name: 'start', type: Boolean },
+              { name: 'stop', type: Boolean },
+              { name: 'restart', type: Boolean },
+              { name: 'json', alias: 'j', type: String },
+              { name: 'tag', alias: 't', type: String },
+              { name: 'monitor', alias: 'm',type: Boolean }
+            ]
+            const commandLineArgs = require('command-line-args')
+            const options = commandLineArgs(optionDefinitions);
+            if(typeof options.json != "undefined") {
+                var docname = "file";
+                if(typeof options.tag != "undefined") docname=options.tag;
+                var updateFile=function() {
+                  var src =JSON.parse(fs.readFileSync(options.json));
+                  if(typeof src._id != "undefined") delete src._id;
+                  if(typeof src._rev != "undefined") delete src._rev;
+                  nodedb.upsert(docname,function(orgdoc) {
+                          return src;
+                  }).then(function() {
+                      if(typeof options.monitor != "undefined") {
+                        fs.watchFile(options.json,function() {
+                          updateFile();
+                        });
+                      }
+                  });
+                }
+                updateFile();
+            }
+
+          }).catch(function (err) {
+            service(function() {
+                bootstrap();
+            });
+          });
+  }
+  bootstrap();
+
 });
