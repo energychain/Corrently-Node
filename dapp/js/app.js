8ebb438f28ba5e0a9da277789c59b1dd5678369f
Thorsten Zoerner <me@thorsten-zoerner.com>
2018-08-18 03:14:57 +0200

    Added general charts to DAPP

--- dapp/js/app.js
+++ dapp/js/app.js
@@ -2,17 +2,12 @@ function previewDoc(dbname,docid) {
   var db = new PouchDB('http://'+location.host+'/'+dbname);
   db.get(docid).then(function(doc) {
       var html="<h3>"+docid+"</h3>";
+
       if(typeof doc.values != "undefined") {
-        var datasets=[];
-        var data = [];
-        labels = [];
-        var v = doc.values;
-        $.each(doc.values,function(key,value) {
-
-
-        });        
+				html+="<canvas id='canvas'></canvas>";
       }
       $.each(doc,function(key,value) {
+					if(key!="values") {
             html+='<div class="row">';
             html+='<div class="col-6">';
             html+=key;
@@ -27,9 +22,90 @@ function previewDoc(dbname,docid) {
             }
             html+='</div>';
             html+="</div>";
+					}
       });
       $('#preview_'+dbname).html(html);
+			if(typeof doc.values != "undefined") {
+				var datasets={};
+				var data = [];
+				var colors=["#5BC0EB","#FDE74C","#9BC53D","#C3423F","#404E4D","#D4AA7D","#EFD09E","#D2D8B3"];
+				labels = [];
+				var v = doc.values;
+				var last_position='right';
+				$.each(doc.values,function(key,value) {
+						$.each(value,function(k,v) {
+							if(k=="ts") {
+									//labels.push(moment(new Date(v*1)).format('D.M.Y h:m'));
+									//labels.push(v*1);
+									labels.push(new Date(v*1));
+							}
 
+							if(typeof datasets[k] == "undefined") {
+								if(last_position=='right') last_position='left'; else last_position='right';
+								var color=colors.pop();
+								datasets[k]={};
+								datasets[k].label=k;
+								datasets[k].fill=false;
+								datasets[k].data=[];
+								datasets[k].yAxisID= 'y-axis-'+k;
+								datasets[k].backgroundColor=color
+								datasets[k].borderColor=color
+								datasets[k].scaleY={
+											type: 'linear',
+											display: true,
+											position: last_position,
+											id: 'y-axis-'+k,
+								}
+							}
+							//datasets[k].data.push({y:v,t:new Date(value.ts),x:value.ts*1});
+							datasets[k].data.push(v*1);
+						});
+				});
+				var chart_datasets=[];
+				var yaxis=[];
+				$.each(datasets,function(key,value) {
+					if(key!="ts") {
+						chart_datasets.push(value);
+						yaxis.push(value.scaleY);
+						}
+				});
+				var ctx = document.getElementById('canvas').getContext('2d');
+				var myLineChart = new Chart.Line(ctx, {
+						type: 'line',
+						data:{ datasets: chart_datasets,
+								labels:labels
+						},
+						options:{
+									responsive: true,
+									hoverMode: 'index',
+									stacked: false,
+									title: {
+										display: true,
+										text: 'Timeseries Plot '+docid
+									},
+									scales: {
+											yAxes:yaxis,
+											xAxes: [{
+			                    type: 'time',
+			                    display: true,
+													scaleLabel: {
+					                    display: true,
+					                    labelString: "Date",
+                					}
+			                }]
+									},
+									pan: {
+					            enabled: true,
+					            mode: 'xy'
+					        },
+
+					        zoom: {
+					            enabled: true,
+					            mode: 'x',
+					        }
+						}
+					});
+			}
   });
 }
 function fetchAllDocIds(dbname) {
@@ -39,9 +115,15 @@ function fetchAllDocIds(dbname) {
       attachments: false
     }).then(function (result) {
       var html="";
+			html+='<div class="dropdown">';
+  		html+='<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdown'+dbname+'" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">';
+    	html+="Select to preview";
+  		html+="</button>";
+  		html+='<div class="dropdown-menu" aria-labelledby="dropdown'+dbname+'">';
       $.each(result.rows,function(key,value) {
-          html+="<li><a href='#' class='btn' id='preview_"+dbname+"_"+value.id+"' onclick='previewDoc(\""+dbname+"\",\""+value.id+"\")'>"+value.id+"</a></li>";
+          html+="<a href='#' class='dropdown-item' id='preview_"+dbname+"_"+value.id+"' onclick='previewDoc(\""+dbname+"\",\""+value.id+"\")'>"+value.id+"</a>";
       });
+				html+="</div></div>";
       $('#docs_'+dbname).append(html);
   });
 }
@@ -58,6 +140,7 @@ function createNodeInfo(dbname) {
       html+="</div>";
       html+='<div class="card-body collapse" id="card-body-'+dbname+'">';
       $.each(doc,function(key,value) {
+						if(key!="verifications") {
             html+='<div class="row">';
             html+='<div class="col-6">';
             html+=key;
@@ -66,6 +149,7 @@ function createNodeInfo(dbname) {
             html+=value;
             html+='</div>';
             html+="</div>";
+						}
       });
       html+='<div class="row">';
       html+='<div class="col-6">';
@@ -100,7 +184,7 @@ function createNodeInfo(dbname) {
     html+='</div>';
     html+='</div>';
     html+="<hr/>";
-    html+="<div id='preview_"+dbname+"' class='bg-secondary'></div>";
+    html+="<div id='preview_"+dbname+"' class='bg-light'></div>";
     html+="</div>";
     html+="</div>";
     $('#app').append(html);
@@ -112,8 +196,8 @@ function createNodeInfo(dbname) {
 
 $(document).ready(function() {
     $.getJSON('http://'+location.host+'/_all_dbs',function(data) {
-      console.log(data);
         $.each(data,function(key,value) {
+							if((value!="_users")&&(value!="_replicator"))
               createNodeInfo(value);
         });
     });
